a1c706c73c9531939c42d8acddbc2079c3fddd4d
dipanshu283 <en20cs301137@medicaps.ac.in>
2022-01-02 18:01:01 +0530

    covid

--- src/App.js
+++ src/App.js
@@ -5,29 +5,34 @@ import { useState } from "react";
 import Weather from "./component/weather/Weather";
 import Talk from "./component/letstalk/Talk";
 import News from "./component/news/News";
+import CovidState from "./component/covid/Covid";
 
 function App() {
-
-
-  const [weatherinfo , setwetherinfo ] = useState([]); 
+  const [weatherinfo, setwetherinfo] = useState([]);
   const [newsArticles, setnewsArticles] = useState([]);
+  const [hdata, sethdata] = useState([]);
 
   useEffect(() => {
     const key =
       "e45f3f500923192b166c768b47a6388f2e956eca572e1d8b807a3e2338fdd0dc/stage";
     alanBtn({
       key: key,
-      onCommand: ({ command , citty , articles}) => {
+      onCommand: ({ command, citty, articles }) => {
         if (command === "giveweather") {
           // setnewsArticles(articles);
           fetched(citty);
-        } 
-        if(command==="newHeadLines"){
+        }
+        if (command === "givecovid") {
+          // setnewsArticles(articles);
+          fetchData();
+        }
+        if (command === "newHeadLines") {
           setnewsArticles(articles);
         }
-        if(command==="GoBack"){
+        if (command === "GoBack") {
           setnewsArticles([]);
           setwetherinfo([]);
+          sethdata([]);
         }
       },
       // onCommand: ({ command , articles}) => {
@@ -36,29 +41,35 @@ function App() {
       //     console.log(articles);
       //   }
       // },
-    })
-  },[])
+    });
+  }, []);
 
+  const fetched = async (citty) => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?appid=715e73f6632ecedac1c05aec0c1990e3&q=${citty}`;
 
- const fetched = async(citty)=>{
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+    setwetherinfo(data);
+    return data;
+  };
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?appid=715e73f6632ecedac1c05aec0c1990e3&q=${citty}`
+  const fetchData = async () => {
+    const urll = "https://data.covid19india.org/data.json";
+    const responsee = await fetch(urll);
+    let dataa = await responsee.json();
+    dataa = dataa.statewise;
+    console.log(dataa);
+    sethdata(dataa);
+  };
 
-  let response = await fetch(url);
-  let data =  await response.json();
-  console.log(data);
-  setwetherinfo(data);
-  return data;
-  }
   return (
     <>
-   
       <div className="home_page">
-      <Weather info= {weatherinfo}/>
-      <Talk/>
-      <News articles={newsArticles} />
-   
-
+        <Weather info={weatherinfo} />
+        <Talk />
+        <News articles={newsArticles} />
+        <CovidState hdata={hdata} />
       </div>
     </>
   );
---
+++ src/component/covid/Covid.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import "./Covid.css";
+
+function CovidState({hdata}) {
+  const newsimg = require("./imgcovid.png");
+  console.log("hl",hdata);
+  if (!hdata.length){
+    return (
+      <>
+        <div className="covid_wrapper">
+          <div className="infoo">
+            <h1 className="bold">"show/give me the covid updates"</h1>
+          </div>
+          <img className="imgggg" src={newsimg} alt="" />
+
+        </div>
+      </>
+    );
+  }
+else{
+  return (
+    <>
+      <div className="covid_wrapper">
+          <div className="infoo">
+            <h1 className="bold">"here is a covid deshboard"</h1>
+          </div>
+          <img className="imgggg" src={newsimg} alt="" />
+
+        </div>
+    <div className="container">
+      <h1 className="headingg">
+        <span className="thick-heading IN">IN</span>
+        <span className=" thick-heading D">D</span>
+        <span className=" thick-heading IA">IA </span>
+        COVID-19 Dashboard
+      </h1>
+
+
+<div className="table-responsive tablee">
+      <table className="table table-hover ">
+
+      <thead className="table-dark">
+        <tr className="dark">
+          <th>State</th>
+          <th>Confirm</th>
+          <th>Recovered</th>
+          <th>Deaths</th>
+          <th>Active</th>
+          <th>Update</th>
+        </tr>
+        </thead>
+
+        <tbody className="table-body">
+      
+       
+       {hdata.map((value, index)=>{
+           console.log(value.state);
+           return(
+        <tr key={index}>
+            <td>{value.state}</td>
+            <td>{value.confirmed}</td>
+            <td>{value.recovered}</td>
+            <td>{value.deaths}</td>
+            <td>{value.active}</td>
+            <td>{value.lastupdatedtime}</td>
+          </tr>
+           );
+       })}
+         
+        </tbody>
+      </table>
+      </div>
+    </div>
+    </>
+  );
+}
+}
+export default CovidState;
